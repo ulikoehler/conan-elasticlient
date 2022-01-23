2c0dc774ee5e009ab4f35301f13575055b8445ee
Uli Köhler <ukoehler@techoverflow.net>
2022-01-23 01:40:57 +0000

    Fix *.so wildcard not capturing *.so.2

--- conanfile.py
+++ conanfile.py
@@ -58,7 +58,7 @@ conan_basic_setup()''')
     def package(self):
         self.copy("*.h", dst="include", src="elasticlient/include")
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
